99d202b4c2b00eb8b01c64131218c53c19e6ee7e
Alejandro Quiles García <alexqg1208@gmail.com>
2024-10-03 14:54:34 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let nombre = ["Pablo", "Marcos", "Marta", "Maria"];
+  let accion = ["se perdió", "se escapó", "se escondió", "estudió"];
+  let lugar = ["en la casa", "en la escuela", "en el templo", "en el mar"];
+  let indiceNombre = Math.floor(Math.random() * nombre.length);
+  let indiceAccion = Math.floor(Math.random() * accion.length);
+  let indiceLugar = Math.floor(Math.random() * lugar.length);
+
+  document.querySelector("#excuse").innerHTML =
+    nombre[indiceNombre] +
+    " " +
+    accion[indiceAccion] +
+    " " +
+    lugar[indiceLugar];
   console.log("Hello Rigo from the console!");
 };
